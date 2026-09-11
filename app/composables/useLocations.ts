c4f2b7c470e1167fee2ddd76e12b936efafb2d6e
Elvis Izuegbu <izuegbuelvis@gmail.com>
2026-09-11 18:20:48 +0100

    commented out unused composable logic

--- app/composables/useLocations.ts
+++ app/composables/useLocations.ts
@@ -106,42 +106,42 @@ export function useDymanicLocationInfo(slug: MaybeRefOrGetter<string>) {
   return { location, isLocationPending, locationError, refreshLocation };
 }
 
-export function getLocationDetail(slug: MaybeRefOrGetter<string>) {
-  const supabase = useSupabaseClient<Database>();
+// export function getLocationDetail(slug: MaybeRefOrGetter<string>) {
+//   const supabase = useSupabaseClient<Database>();
 
-  const {
-    data: location,
-    pending: isLocationPending,
-    error: locationError,
-  } = useAsyncData(
-    () => `location-detail-${toValue(slug)}`,
-    async () => {
-        const locationSlug = toValue(slug);
+//   const {
+//     data: location,
+//     pending: isLocationPending,
+//     error: locationError,
+//   } = useAsyncData(
+//     () => `location-detail-${toValue(slug)}`,
+//     async () => {
+//         const locationSlug = toValue(slug);
 
-      if (!locationSlug) return null;
+//       if (!locationSlug) return null;
 
-      const { data, error } = await supabase
-        .from("locations")
-        .select(
-          `
-          id,
-          slug,
-          location,
-          city,
-          price_multiplier,
-          open_time,
-          close_time
-        `,
-        )
-        .eq("slug", locationSlug)
-        .single();
+//       const { data, error } = await supabase
+//         .from("locations")
+//         .select(
+//           `
+//           id,
+//           slug,
+//           location,
+//           city,
+//           price_multiplier,
+//           open_time,
+//           close_time
+//         `,
+//         )
+//         .eq("slug", locationSlug)
+//         .single();
 
-      if (error) throw error;
+//       if (error) throw error;
 
-      return data;
-    },
-    { watch: [() => toValue(slug)] },
-  );
+//       return data;
+//     },
+//     { watch: [() => toValue(slug)] },
+//   );
 
-  return { location, isLocationPending, locationError };
-}
+//   return { location, isLocationPending, locationError };
+// }
