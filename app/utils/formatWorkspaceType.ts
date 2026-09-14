79aa25ccc97624db35b1882854577295d235e33c
Elvis Izuegbu <izuegbuelvis@gmail.com>
2026-09-14 16:15:46 +0100

    update: remove old formater util

--- app/utils/formatWorkspaceType.ts
+++
@@ -1,8 +0,0 @@
-
-//format workspace type 
-export function formatWorkspaceType(type: string): string {
-  return type
-    .split("_")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
-}
